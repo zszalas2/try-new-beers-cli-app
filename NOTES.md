e69766899bdfb913ddc0396cdc9f5b2d0692f2c1
zszalas2 <zszalas@gmail.com>
2017-01-16 21:25:18 -0500

    CLI flow w/o real data

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+1. File structure
+2. Create bin and environment
+3. work out the interface
--- lib/try-new-beers/cli.rb
+++ lib/try-new-beers/cli.rb
@@ -1,7 +1,36 @@
 class TryNewBeers::CLI
 
   def call
-    puts "Here are the 25 most recently reviewed beers:"
+    list_beers
+    menu
+    goodbye
   end
-  
+
+  def list_beers
+    puts "Here are the 25 latest reviewed beers:"
+    puts <<-DOC
+    1. Doghead - IPA - Avg Review:4.25
+    2. FunnyBone - Lager - Avg Review: 3.75
+    3. XYZ - Pale Ale - Avg Review: 4.15
+    DOC
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the beer you'd like to learn more about"
+      input = gets.strip.downcase
+      case input 
+      when "1"
+        "Great choice! Here's some more info"
+      when "2"
+        "Great choice! Here's some more info"
+      when "3"
+        "Great choice! Here's some more info"
+      when "list"
+        "list_beers"
+      else 
+        puts "Not sure what you're asking for."
+      end
+    end
 end
