404b1af4baf978a3a3aee00147c8d589a6d98811
zszalas2 <zszalas@gmail.com>
2017-02-18 13:35:24 -0500

    cleaned up code

--- lib/try-new-beers/beer_review.rb
+++ lib/try-new-beers/beer_review.rb
@@ -30,18 +30,14 @@ class BeerReview
   end
 
   def self.names
-    #extract name
     @names ||= @doc.search("tr a b").collect {|e| e.text}
   end
 
   def self.types
-    #extract type and brewery
     @types ||= @doc.search("div#extendedInfo a").children.select.each_with_index { |children, i| i.odd? }.collect {|e| e.text}
   end
 
   def self.breweries
-    #I need this to return to me an array with the strings of all the breweries. Because the span for type and brewery are both children of the same,
-    #I have to use even? or odd? on the arra
     @breweries ||= @doc.search("div#extendedInfo a").children.select.each_with_index { |children, i| i.even? }.collect {|e| e.text}
   end
     
@@ -51,23 +47,19 @@ class BeerReview
   end
 
   def self.profiles
-    #I need this code to return the url extension and then attache the extension on the end of every url (last part can be done under scrape_all )
     @profiles ||= @doc.search("tr td a").collect {|link| link.attribute('href').value}
   end
 
 
   def self.get_page
     @doc = Nokogiri::HTML(open("https://www.beeradvocate.com/lists/top/"))
-    #name = doc.search("tr a b").each do |n| puts n.text end
-    #type_and_brewing_company = @doc.search("div#extendedInfo").children.each do |row| puts row.children.first end
   end
 
   def self.scrape_all
     self.get_page
     beer_review_array = names.zip(types, breweries, ratings, profiles)
     
-    (0..beer_review_array.size-1).to_a.each_with_index do |i| #0..bra.size returns 250. Convert to an array, returning an array of integers from 0 - 250. Each_index will iterate over every single integer. Using i as a variable for every integer
-      beer_review = BeerReview.new
+    (0..beer_review_array.size-1).to_a.each_with_index do |i|
       beer_review.name = beer_review_array[i][0]
       beer_review.type = beer_review_array[i][1]
       beer_review.brewery = beer_review_array[i][2]
--- lib/try-new-beers/cli.rb
+++ lib/try-new-beers/cli.rb
@@ -1,15 +1,6 @@
 require 'pry'
 class TryNewBeers::CLI
 
-  
-
-
-  #Greet the user - show picture for cheer (beer mug)...loading .....
-  #Retrieve reviews
-  #Instruct on browsing or searching- it should list all the beers from 1 - 250 
-  # give the option to search by type
-  # take input and return beer type
-
 
   def call
 
@@ -55,46 +46,14 @@ class TryNewBeers::CLI
     end
   end
 
-  #def print_beer_review
-    #this should print out all the beers that are a certain type
-    #beer = BeerReview.find_by_type(input)
-    #puts ""
-    #puts "#{i}. #{b.name}, #{b.type}, #{b.brewery}, #{b.rating}, https://www.beeradvocate.com#{beer.profile}"
-  #end
-
-  #def see_more(input)
-    #this method should allow user to navigate to the profile of the beer of their choice based on index, 
-    #take input
-    #convert index to integer 
-    #take that index' profile and navigate to www.beeradvocate.com/#{profile}/// openuri??
-  #end
-
-
 
   def menu
-    #menu should have two options: search by type or see more of a particular beer OR list again
-    #is someone picks searc- they are able to input the type of beer they like and that will return all beers that match that value
     puts "Hi there... didn't see you come in. Insert the number of the beer would you like to explore?"
     input = gets.strip.to_i
-
-    #if input == "type"
-      #puts "What type of beer would you like to explore?"
-      #input = gets.strip.downcase
-      #BeerReview.find_by_type(input)
 
       beer = BeerReview.find_by_index(input.to_i-1)
       `open #{"https://www.beeradvocate.com#{beer.profile}"}`
 
-    if input == list
-      list        
-    else puts "I don't know what you want. Either input 'number' to search by beer number, 'type' to search by beer type, 'list' to see the list of beers, or 'exit' to quit."
-      input 
-     #   see_more
-      #elsif input == "list"
-       # list
-      #else 
-       # puts "Not sure what you're asking for."
-     # end
     end
   end
 end
